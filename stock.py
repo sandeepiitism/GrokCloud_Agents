ec139f5a52619b0c7518c3f733e07022e6b5bd19
Sandeep Chowdhury <49147378+sandeepiitism@users.noreply.github.com>
2025-02-06 16:58:57 +0530

    Add files via upload

---
+++ stock.py
@@ -0,0 +1,52 @@
+"""Run `pip install yfinance` to install dependencies."""
+
+from phi.agent import Agent
+from phi.model.groq import Groq
+from phi.tools.yfinance import YFinanceTools
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def get_company_symbol(company: str) -> str:
+    """Use this function to get the symbol for a company.
+
+    Args:
+        company (str): The name of the company.
+
+    Returns:
+        str: The symbol for the company.
+    """
+    symbols = {
+        "Phidata": "MSFT",
+        "Infosys": "INFY",
+        "Tesla": "TSLA",
+        "Apple": "AAPL",
+        "Microsoft": "MSFT",
+        "Amazon": "AMZN",
+        "Google": "GOOGL",
+    }
+    return symbols.get(company, "Unknown")
+
+
+GROQ_API_KEY = "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"
+
+# agent = Agent(
+#     model=Groq(id="deepseek-r1-distill-llama-70b", api_key=GROQ_API_KEY)
+# )
+
+agent = Agent(
+    model=Groq(id="deepseek-r1-distill-llama-70b", api_key=GROQ_API_KEY),
+    tools=[YFinanceTools(stock_price=True, analyst_recommendations=True, stock_fundamentals=True), get_company_symbol],
+    instructions=[
+        "Use tables to display data.",
+        "If you need to find the symbol for a company, use the get_company_symbol tool.",
+    ],
+    show_tool_calls=True,
+    markdown=True,
+    debug_mode=True,
+)
+
+agent.print_response(
+    "Summarize and compare analyst recommendations and fundamentals for TSLA and Phidata. Show in tables.", stream=True
+)
